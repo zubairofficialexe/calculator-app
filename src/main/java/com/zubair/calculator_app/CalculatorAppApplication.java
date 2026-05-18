7a8e5350cf47cd7112e794021568cc1f8f56c9fd
Zubair <Zubair@gmail.com>
2026-05-18 15:00:47 +0530

    Folder structure is defined

---
+++ src/main/java/com/zubair/calculator_app/CalculatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.zubair.calculator_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zubair/calculator_app/CalculatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubair.calculator_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
